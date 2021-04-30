c22f3b94b5171513c9c2ca56e5fb0218dd19d6d6
JustLink <linki_mail@mail.ru>
2021-04-30 22:00:36 +0700

    Создан проект OnlinkServer

---
+++ Onlink/OnlinkServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OnlinkServer
+{
+    class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
